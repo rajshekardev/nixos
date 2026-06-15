70a7c6348d0bb87ead76082d565aef42f4587648
Raj Shekar S <rajshekarwork@gmail.com>
2026-06-15 13:07:01 +0530

    UTF-8 fix5

--- configuration.nix
+++ configuration.nix
@@ -21,20 +21,18 @@
 
   time.timeZone = "Asia/Kolkata";
 
-  i18n.defaultLocale = "en_IN.UTF-8";
-
-  i18n.extraLocales = [ "en_IN.UTF-8/UTF-8" ];
+  i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IN.UTF-8";
-    LC_IDENTIFICATION = "en_IN.UTF-8";
-    LC_MEASUREMENT = "en_IN.UTF-8";
-    LC_MONETARY = "en_IN.UTF-8";
-    LC_NAME = "en_IN.UTF-8";
-    LC_NUMERIC = "en_IN.UTF-8";
-    LC_PAPER = "en_IN.UTF-8";
-    LC_TELEPHONE = "en_IN.UTF-8";
-    LC_TIME = "en_IN.UTF-8";
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
   services.xserver = {
