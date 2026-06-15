8541f7b351985fb6126bda4cb6abb2823e8568ab
Raj Shekar S <rajshekarwork@gmail.com>
2026-06-15 17:35:36 +0530

    back to xp-pen 4

--- configuration.nix
+++ configuration.nix
@@ -193,7 +193,9 @@
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
     ELECTRON_OZONE_PLATFORM_HINT = "x11";
+    QT_QPA_PLATFORM = "wayland;xcb";
   };
+
   virtualisation = {
 
     waydroid.enable = true;
