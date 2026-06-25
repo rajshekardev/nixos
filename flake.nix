f81ab7aecd7d223f7e041844e0ae05890003a6ec
rajshekar <rajshekarwork@gmail.com>
2026-06-25 07:59:06 +0530

    cache fix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
     description = "NixOS + Niri";
 
     inputs = {
-      nixpkgs.url = "nixpkgs/nixos-26.05";
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
       home-manager = {
           url = "github:nix-community/home-manager/release-26.05";
           inputs.nixpkgs.follows = "nixpkgs";
